9e202287cd703fda3fb320f4f3960fd4d0e073a3
Seyi Abiodun <cypher04>
2026-05-04 04:53:30 +0100

    Feature(Private endpoint): Added private endpoint for ACR.

--- modules/private_endpoint/main.tf
+++ modules/private_endpoint/main.tf
@@ -28,3 +28,41 @@ resource "azurerm_private_dns_zone_virtual_network_link" "private_dns_zone_link"
   
 # }
 
+// create private endpoint for container registry
+
+resource "azurerm_private_endpoint" "container_registry" {
+  name                = "${var.resource_group_name}-acr-pe"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  subnet_id           = var.subnet_ids["app"]
+
+  private_service_connection {
+    name                           = "${var.resource_group_name}-acr-psc"
+    private_connection_resource_id = var.acr_id
+    subresource_names              = ["registry"]
+    is_manual_connection           = false
+  }
+
+
+  private_dns_zone_group {
+    name                 = "${var.resource_group_name}-acr-dnszonegroup"
+    private_dns_zone_ids = [azurerm_private_dns_zone.acr_dns_zone.id]
+  }
+
+}
+
+resource "azurerm_private_dns_zone" "acr_dns_zone" {
+  name                = "privatelink.azurecr.io"
+  resource_group_name = var.resource_group_name
+  
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "acr_dns_zone_link" {
+  name                  = "${var.resource_group_name}-acr-dnszone-link"
+  resource_group_name   = var.resource_group_name
+  private_dns_zone_name = azurerm_private_dns_zone.acr_dns_zone.name
+  virtual_network_id    = var.vnet_id
+  
+  depends_on = [ var.subnet_prefixes]
+}
+
